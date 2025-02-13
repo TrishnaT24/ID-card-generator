d89a0e2fcf78cd453ad94842015add58b7826c8d
TrishnaT24 <tiwaritrishna07@gmail.com>
2025-02-13 17:21:12 +0530

    final

--- my-vite-app/backend/server.js
+++ my-vite-app/backend/server.js
@@ -8,7 +8,12 @@ const connectDB=require('./config/db');
 app.use(express.json());
 // app.use(cors({ origin: "http://localhost:5173" }));
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "https://id-card-generator-frontend.vercel.app", // ✅ Replace with your frontend URL
+  methods: ["GET", "POST", "PUT", "DELETE"], // ✅ Allowed methods
+  credentials: true, // ✅ Allow cookies if needed
+}));
 connectDB();
 app.use("/api",userRoutes);
 // Start server
